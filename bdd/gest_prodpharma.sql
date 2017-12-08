345e3545641803df848696f8f2c8a67eb7880bfb
gael243 <gmapwata6@yahoo.fr>
2017-12-08 22:43:51 +0100

    base de donnee corriger

--- bdd/gest_prodpharma.sql
+++ bdd/gest_prodpharma.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Serveur: 127.0.0.1
--- Généré le : Mar 05 Décembre 2017 à 01:26
+-- Généré le : Jeu 07 Décembre 2017 à 10:19
 -- Version du serveur: 5.5.8
 -- Version de PHP: 5.3.5
 
@@ -22,122 +22,101 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 -- --------------------------------------------------------
 
 --
--- Structure de la table `article`
+-- Structure de la table `articles`
 --
 
-CREATE TABLE IF NOT EXISTS `article` (
-  `id_articles` int(11) NOT NULL,
-  `lib_articles` varchar(100) NOT NULL,
-  `id_fournisseur` varchar(120) NOT NULL,
-  `id_categorie` int(11) NOT NULL,
-  PRIMARY KEY (`id_articles`),
-  KEY `FK_Article_id_fournisseur` (`id_fournisseur`),
-  KEY `FK_Article_id_categorie` (`id_categorie`)
+CREATE TABLE IF NOT EXISTS `articles` (
+  `articles_id` int(11) NOT NULL,
+  `label` varchar(100) NOT NULL,
+  `fournisseur_id` varchar(120) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  PRIMARY KEY (`articles_id`),
+  KEY `FK_Articles_fournisseur_id` (`fournisseur_id`),
+  KEY `FK_Articles_category_id` (`category_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Contenu de la table `article`
+-- Contenu de la table `articles`
 --
 
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `categorie`
+-- Structure de la table `categories`
 --
 
-CREATE TABLE IF NOT EXISTS `categorie` (
-  `id_categorie` int(11) NOT NULL,
-  `lib_categorie` varchar(25) NOT NULL,
-  PRIMARY KEY (`id_categorie`)
+CREATE TABLE IF NOT EXISTS `categories` (
+  `category_id` int(11) NOT NULL,
+  `label` varchar(25) NOT NULL,
+  PRIMARY KEY (`category_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Contenu de la table `categorie`
+-- Contenu de la table `categories`
 --
 
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `client`
+-- Structure de la table `clients`
 --
 
-CREATE TABLE IF NOT EXISTS `client` (
-  `id_client` varchar(100) NOT NULL,
-  `Nom` varchar(100) NOT NULL,
+CREATE TABLE IF NOT EXISTS `clients` (
+  `client_id` varchar(100) NOT NULL,
+  `nom` varchar(100) NOT NULL,
   `telephone` varchar(100) NOT NULL,
-  `Adresse_physique` varchar(100) NOT NULL,
+  `adresse` varchar(100) NOT NULL,
   `sexe_client` varchar(10) DEFAULT NULL,
   `ville_provenance` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`id_client`)
+  `stock_id` varchar(100) NOT NULL,
+  PRIMARY KEY (`client_id`),
+  KEY `FK_Clients_stock_id` (`stock_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Contenu de la table `client`
+-- Contenu de la table `clients`
 --
 
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `fournisseur`
+-- Structure de la table `fournisseurs`
 --
 
-CREATE TABLE IF NOT EXISTS `fournisseur` (
-  `id_fournisseur` varchar(120) NOT NULL,
-  `lib_fournisseur` varchar(100) NOT NULL,
-  `Adresse` varchar(100) NOT NULL,
-  `Telephone` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`id_fournisseur`)
+CREATE TABLE IF NOT EXISTS `fournisseurs` (
+  `fournisseur_id` varchar(120) NOT NULL,
+  `label` varchar(100) NOT NULL,
+  `adresse` varchar(100) NOT NULL,
+  `telephone` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`fournisseur_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Contenu de la table `fournisseur`
+-- Contenu de la table `fournisseurs`
 --
 
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `paiement`
+-- Structure de la table `stocks`
 --
 
-CREATE TABLE IF NOT EXISTS `paiement` (
-  `id_paie` varchar(25) NOT NULL,
-  `date_paiement` date DEFAULT NULL,
-  `Quantite_vendus` int(11) NOT NULL,
-  `prix_total` int(11) DEFAULT NULL,
-  `id_stock` varchar(100) NOT NULL,
-  `id_client` varchar(100) NOT NULL,
-  PRIMARY KEY (`id_paie`),
-  KEY `FK_paiement_id_stock` (`id_stock`),
-  KEY `FK_paiement_id_client` (`id_client`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Contenu de la table `paiement`
---
-
-
--- --------------------------------------------------------
-
---
--- Structure de la table `stock`
---
-
-CREATE TABLE IF NOT EXISTS `stock` (
-  `id_stock` varchar(100) NOT NULL,
-  `lib_stock` varchar(120) NOT NULL,
+CREATE TABLE IF NOT EXISTS `stocks` (
+  `stock_id` varchar(100) NOT NULL,
+  `label` varchar(120) NOT NULL,
   `quantite` int(11) NOT NULL,
   `prix_unitaire` int(11) NOT NULL,
-  `id_articles` int(11) NOT NULL,
-  PRIMARY KEY (`id_stock`),
-  KEY `FK_stock_id_articles` (`id_articles`)
+  `articles_id` int(11) NOT NULL,
+  PRIMARY KEY (`stock_id`),
+  KEY `FK_stocks_articles_id` (`articles_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Contenu de la table `stock`
+-- Contenu de la table `stocks`
 --
 
 
@@ -146,21 +125,20 @@ CREATE TABLE IF NOT EXISTS `stock` (
 --
 
 --
--- Contraintes pour la table `article`
+-- Contraintes pour la table `articles`
 --
-ALTER TABLE `article`
-  ADD CONSTRAINT `FK_Article_id_categorie` FOREIGN KEY (`id_categorie`) REFERENCES `categorie` (`id_categorie`),
-  ADD CONSTRAINT `FK_Article_id_fournisseur` FOREIGN KEY (`id_fournisseur`) REFERENCES `fournisseur` (`id_fournisseur`);
+ALTER TABLE `articles`
+  ADD CONSTRAINT `FK_Articles_category_id` FOREIGN KEY (`category_id`) REFERENCES `categories` (`category_id`),
+  ADD CONSTRAINT `FK_Articles_fournisseur_id` FOREIGN KEY (`fournisseur_id`) REFERENCES `fournisseurs` (`fournisseur_id`);
 
 --
--- Contraintes pour la table `paiement`
+-- Contraintes pour la table `clients`
 --
-ALTER TABLE `paiement`
-  ADD CONSTRAINT `FK_paiement_id_client` FOREIGN KEY (`id_client`) REFERENCES `client` (`id_client`),
-  ADD CONSTRAINT `FK_paiement_id_stock` FOREIGN KEY (`id_stock`) REFERENCES `stock` (`id_stock`);
+ALTER TABLE `clients`
+  ADD CONSTRAINT `FK_Clients_stock_id` FOREIGN KEY (`stock_id`) REFERENCES `stocks` (`stock_id`);
 
 --
--- Contraintes pour la table `stock`
+-- Contraintes pour la table `stocks`
 --
-ALTER TABLE `stock`
-  ADD CONSTRAINT `FK_stock_id_articles` FOREIGN KEY (`id_articles`) REFERENCES `article` (`id_articles`);
+ALTER TABLE `stocks`
+  ADD CONSTRAINT `FK_stocks_articles_id` FOREIGN KEY (`articles_id`) REFERENCES `articles` (`articles_id`);
