c46cef7bf2bc9313e2a19ae145f55777ce478fbf
DedrickEnc <kitamuka@gmail.com>
2017-12-13 18:51:07 +0100

    test (integration) fixing the breaking issue about integration test

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 const expect = require('chai').expect;
 
 
-describe('Essaie', () => {
+describe('Essaie de test prototype', () => {
     it('test succefully', () => {
         const x = 1
         expect(x).to.equal(1);
